2e7765d7cd3165a9fd72c09e7aa3261051e49e5a
Harsh Bhatia <harshbhatia2007@gmail.com>
2020-08-11 03:13:09 +0530

    added new component

---
+++ test-proj/src/app/product-1/product-1.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product1',
+  template: `<h2>This is component for product 1</h2>`
+})
+
+export class ProductOneComponent { }
